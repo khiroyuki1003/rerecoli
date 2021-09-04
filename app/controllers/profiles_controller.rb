68f3c56a83a53165f63bef6644674e6501323b46
tokorotenn <hk.1003.abc@gmail.com>
2021-09-04 16:01:27 +0900

    プロフィール新規作成機能

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,9 +1,35 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user, only: [:new, :create, :show, :edit, :update]
+  before_action :user_profile_check, only: [:new, :create]
+
   def new
+    @profile = Profile.new
   end
 
   def create
+    @profile = Profile.new(profile_params)
+    if @profile.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
+  private
+
+  def set_user
+    @user = User.find(current_user.id)
+  end
+
+  def user_profile_check
+    if @user.profile.present?
+      redirect_to root_path
+    end
+  end
+
+  def profile_params
+    params.require(:profile).permit(:nickname, :precious_word, :birth_date).merge(user: current_user)
   end
 end
  
