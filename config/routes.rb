516f1446b759ecbdfac4e1a1ab313b21b1360c27
tokorotenn <hk.1003.abc@gmail.com>
2021-09-04 16:14:43 +0900

    ユーザー登録後プロフィール登録へ遷移

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: "users/sessions", 
+    registrations: "users/registrations"
+    } 
   root to: "categories#index"
 
   resources :profiles, only: [:new, :create]
