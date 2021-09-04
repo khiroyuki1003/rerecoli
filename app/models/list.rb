4987c15cd7d0662992308911bf1f16de7d08f115
tokorotenn <hk.1003.abc@gmail.com>
2021-09-04 20:34:21 +0900

    バリデーション追加

--- app/models/list.rb
+++ app/models/list.rb
@@ -5,4 +5,7 @@ class List < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :priority
   
-end
+  validates :list_title, presence: true, length: {
+    maximum: 100, message:"is invalid. Please enter at least 100 characters"
+   }
+end 
