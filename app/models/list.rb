d916bce0ddcdcfc2cdf41bad47a49445766c3ffa
tokorotenn <hk.1003.abc@gmail.com>
2021-09-04 18:49:29 +0900

    アクティブハッシュ作成

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,5 +2,7 @@ class List < ApplicationRecord
   belongs_to :category
   has_one :list_deadline, dependent: :destroy
   has_many :list_reminds, dependent: :destroy
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :priority
+  
 end
- 
