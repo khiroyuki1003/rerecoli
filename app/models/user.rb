a5a2a9618e9c7afe858cb1fcb44c6bf611a4d663
tokorotenn <hk.1003.abc@gmail.com>
2021-09-04 14:52:02 +0900

    パスワードバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :password, format: {with: /\A[a-zA-Z0-9]+\z/, message:"is invalid. Half-width alphanumeric only"}
 end
