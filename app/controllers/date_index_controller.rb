c29da0f962d70fcd919d137b7a5b7b5145adf3f4
tokorotenn <hk.1003.abc@gmail.com>
2021-10-08 14:47:53 +0900

    リファクタリング

--- app/controllers/date_index_controller.rb
+++ app/controllers/date_index_controller.rb
@@ -3,11 +3,13 @@ class DateIndexController < ApplicationController
   before_action :get_user
 
   def unfinished
+    # 達成目標日を基準に時系列で一覧表示
     @deadlines = ListDeadline.where(profile_id: @user.profile.id, status: "unfinished").order(:list_deadline_date).order(list_deadline_time: "ASC")
     @list_reminds = ListRemind.where(profile_id: @user.profile.id, status: "unfinished").order(:list_remind_date).order(list_remind_time: "ASC")
   end
 
   def done
+    # 達成目標日を基準に時系列で一覧表示
     @deadlines = ListDeadline.where(profile_id: @user.profile.id, status: "done").order(list_deadline_date: "DESC").order(list_deadline_time: "DESC")
     @list_reminds = ListRemind.where(profile_id: @user.profile.id, status: "done").order(list_remind_date: "DESC").order(list_remind_time: "DESC")
   end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -7,15 +7,15 @@ class Profile < ApplicationRecord
 
   with_options presence: true do
     validates :nickname, length: {
-       maximum: 20, message:"is invalid. Please enter at least 20 characters"
-      }
+      maximum: 20, message:"is invalid. Please enter at least 20 characters"
+    }
     validates :birth_date
   end
 
   validates :nickname, uniqueness: { case_sensitive: false, message: "is invalid. This nickname is already in use" }
 
   validates :precious_word, length: {
-     maximum: 100, message:"is invalid. Please enter at least 100 characters"
-    }
+    maximum: 100, message:"is invalid. Please enter at least 100 characters"
+  }
 end
   
